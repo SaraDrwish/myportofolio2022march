50c0deee859c70d0a36bd5c5530a9ccae70d9762
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-03-16 03:32:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let section = document.querySelector(".count");
+let spans = document.querySelectorAll(".progress span");
+let span = document.querySelector(".up");
+
+window.onscroll = function(){
+
+    if(this.scrollY >= 1000){
+        span.classList.add("show");
+    }
+    else{
+        span.classList.remove("show")
+    }
+
+// ----------------------------------------------------------
+
+
+    if(window.scrollY >= section.offsetTop - 222 ){
+        // spans.forEach( (span) => {
+        //     span.style.width = span.dataset.width;
+        //   })
+        spans.forEach(function(span) 
+        {
+            span.style.width = span.dataset.width;
+        }
+        
+        );  //end foreach
+    }
+
+
+// ----------------------------------------------------------
+
+
+}
+
+
+span.onclick = function(){
+ 
+   window.scrollTo({
+       top:0 , 
+       behavior: "smooth",
+   });
+
+};
